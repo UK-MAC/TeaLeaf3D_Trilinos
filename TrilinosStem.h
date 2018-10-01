4ab80440075f38105190c92ce8f63c409f6b45ac
douglas-shanks <douglas.j.shanks@bath.edu>
2018-10-01 10:12:25 +0100

    Cleanup headers for 12.12.1

--- TrilinosStem.h
+++ TrilinosStem.h
@@ -18,7 +18,7 @@
 #include <Xpetra_Map.hpp>
 #include <Xpetra_MapFactory.hpp>
 #include <Xpetra_CrsMatrixWrap.hpp>
-#include <Xpetra_VectorFactory.hpp>
+//#include <Xpetra_VectorFactory.hpp>
 #include <Xpetra_MultiVectorFactory.hpp>
 #include <Xpetra_Parameters.hpp>
 #include "BelosOperatorT.hpp"
